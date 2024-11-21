302cef85b2d7479be5d3eb66ef6f8fb0d916c8c8
Will Hoover <70659810+LordGuacaba@users.noreply.github.com>
2024-11-21 14:41:10 -0500

    About and Sources pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -6,6 +6,7 @@ const About = () => {
       <h1>Delaunay Triangulation and Voronoi Diagrams</h1>
       <p>The Delaunay triangulation of a set of points subdivides the polygon formed by their convex hull into a set of triangles whose circumcircles do not contain any other point in the set. As it happens, connecting the centers of those circumcircles with lines produces the Voronoi diagram for that set. Why is that? In this project, we will present a pedagogical aid that shows a dynamic Delaunay triangulation for a set of points and a toggle that will overlay or show the corresponding Voronoi diagram. We will explore the mathematics behind the connection between the two artifacts as well as some of their real-world applications. Our aid will allow viewers to visualize the connection between Delaunay triangulation and Voronoi diagrams and understand their importance.
       </p>
+      <p>This project was created as a final project for CSCI 716 - Computational Geometry at the Rochester Institute of Technology. The link to our final presentation video can be found <a href="https://www.youtube.com/watch?v=N9qYF9DZPdw&pp=ygUPd2hpdGUgYW5kIG5lcmR5">here.</a></p>
       <div>
       </div>
       <footer>
--- src/pages/Sources.tsx
+++ src/pages/Sources.tsx
@@ -1,7 +1,17 @@
 const Sources = () => {
     return (
         <div>
-            <p>Sources</p>
+            <h1>Sources</h1>
+            <p>Much of the knowledge used to build this application was gained from slides presented by Dr. 
+                Reynold Bailey at RIT and drawn from content courtesy of David M. Mount, University of 
+                Maryland and Robert Pless, Washington University in St. Louis.</p>
+            <h3>Other Sources</h3>
+            <ul>
+                <li><a href="https://www.gorillasun.de/blog/bowyer-watson-algorithm-for-delaunay-triangulation/">Bowyer-Watson Algorithm for Delaunay Triangulation</a></li>
+                <li><a href="https://ianthehenry.com/posts/delaunay/">Visualizing Delaunay Triangulation</a></li>
+                <li><a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://medium.com/stamatics-iit-kanpur/voronoi-diagrams-and-delaunay-triangulations-cc57ba901f9e&ved=2ahUKEwiE4O6slO6JAxVlmokEHW3DLPAQFnoECD4QAQ&usg=AOvVaw3KV-TIzIp50sRKqUDyVjPt">Voronoi Diagrams and Delaunay Triangulations</a></li>
+                <li><a href="https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.gorillasun.de/blog/delaunay-triangulation-and-voronoi-diagrams/&ved=2ahUKEwiE4O6slO6JAxVlmokEHW3DLPAQFnoECD0QAQ&usg=AOvVaw1RzjQGr0sl4rhkuNt1mwt4">Delaunay Triangulation and Voronoi Diagrams</a></li>
+            </ul>
         </div>
     )
 }
